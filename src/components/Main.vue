5f4af34806fab081b1e39711f5bd35fa7fcd60f0
lucas-leoni <lucas.leoni09@gmail.com>
2022-04-28 15:57:14 -0300

    update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -90,58 +90,6 @@
           </div>
         </div>
       </div>
-      <!-- <h2>Database</h2>
-      <div id="skills" class="p-3">
-        <h5>MySQL - 65%</h5>
-        <div class="w-100 rounded-pill my-3" id="bg-skill">
-          <div class="rounded-pill" id="skill" style="width: 65%"></div>
-        </div>
-        <h5>SQL Server - 60%</h5>
-        <div class="w-100 rounded-pill my-3" id="bg-skill">
-          <div class="rounded-pill" id="skill" style="width: 60%"></div>
-        </div>
-        <h5>MongoDB - 50%</h5>
-        <div class="w-100 rounded-pill my-3" id="bg-skill">
-          <div class="w-50 rounded-pill" id="skill"></div>
-        </div>
-      </div>
-      <h2>Backend</h2>
-      <div id="skills" class="p-3">
-        <h5>Python - 65%</h5>
-        <div class="w-100 rounded-pill my-3" id="bg-skill">
-          <div class="rounded-pill" id="skill" style="width: 65%"></div>
-        </div>
-        <h5>JavaScript em Node.js - 65%</h5>
-        <div class="w-100 rounded-pill my-3" id="bg-skill">
-          <div class="rounded-pill" id="skill" style="width: 65%"></div>
-        </div>
-      </div> -->
-      <!-- <h2>Frontend</h2>
-      <div id="skills" class="p-3">
-        <h5>HTML - 60%</h5>
-        <div class="w-100 rounded-pill my-3" id="bg-skill">
-          <div class="rounded-pill" id="skill" style="width: 60%"></div>
-        </div>
-        <h5>CSS - 65%</h5>
-        <div class="w-100 rounded-pill my-3" id="bg-skill">
-          <div class="rounded-pill" id="skill" style="width: 65%"></div>
-        </div>
-        <h5>JavaScript - 65%</h5>
-        <div class="w-100 rounded-pill my-3" id="bg-skill">
-          <div class="rounded-pill" id="skill" style="width: 65%"></div>
-        </div>
-      </div>
-      <h2>Versionamento</h2>
-      <div id="skills" class="p-3">
-        <h5>Git - 60%</h5>
-        <div class="w-100 rounded-pill my-3" id="bg-skill">
-          <div class="rounded-pill" id="skill" style="width: 60%"></div>
-        </div>
-        <h5>GitHub - 60%</h5>
-        <div class="w-100 rounded-pill my-3" id="bg-skill">
-          <div class="rounded-pill" id="skill" style="width: 60%"></div>
-        </div>
-      </div> -->
     </div>
     <div v-if="rotaPortfolio" class="container py-5">
       <h2>Portfolio</h2>
