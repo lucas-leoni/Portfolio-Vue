36382e346743cf54f41a5828e4a9dbc8350432cc
lucas-leoni <lucas.leoni09@gmail.com>
2022-04-28 16:26:00 -0300

    update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -91,8 +91,31 @@
         </div>
       </div>
     </div>
-    <div v-if="rotaPortfolio" class="container py-5">
-      <h2>Portfolio</h2>
+    <div v-if="rotaPortfolio" class="container py-5 my-3">
+      <div class="row justify-content-evenly">
+        <div class="col-5">
+          <img src="../assets/project1.jpg" width="260" height="200" />
+          <h6 class="mt-3">Finance</h6>
+          <p class="mb-5">Web development</p>
+        </div>
+        <div class="col-5">
+          <img src="../assets/project2.png" width="260" height="200" />
+          <h6 class="mt-3">Orizon</h6>
+          <p class="mb-5">Web development</p>
+        </div>
+      </div>
+      <div class="row justify-content-evenly">
+        <div class="col-5">
+          <img src="../assets/project3.jpg" width="260" height="200" />
+          <h6 class="mt-3">Fundo</h6>
+          <p class="m-0">Web design</p>
+        </div>
+        <div class="col-5">
+          <img src="../assets/project4.png" width="260" height="200" />
+          <h6 class="mt-3">Brawlhalla</h6>
+          <p class="m-0">Applications</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -130,5 +153,8 @@ i {
 #skill {
   background-color: #1bc4a7;
   height: 1rem;
+}
+img {
+  border-radius: 1rem;
 }
 </style>
